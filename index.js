bdb9d8f57d8ee90ca7769931a1734ace9a5510f5
Ali-Maklad <ali2812002@gmail.com>
2025-02-16 07:35:21 +0200

    updated

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const http = require("http");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
@@ -38,6 +39,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
+server.listen(PORT, () => {
   console.log("Server is running on port 3000");
 });
